49dbf8eada0aa42baba890ef97f09cf99453971c
ewen <ewen.grosjean@gmail.com>
2014-01-20 00:35:20 +0100

    cljs implementation of ring-buffer

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/amalloy/ring-buffer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2138"]])
